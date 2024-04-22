08ad5168cc3e203b7869ac03b2a80dac118e305c
w45sen <w45sen@126.com>
2024-04-22 09:38:06 +0800

    Update MoreContent.vue

--- src/components/MoreContent.vue
+++ src/components/MoreContent.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="more-content">您可在此编写任意内容</div>
+  <div class="more-content">宛安小站寄语</div>
+  <div>孩子出生前曾发愿要做个小站记录孩子成长，恰逢这两年行业惨淡有了闲暇（<del>摸鱼</del>）时间，就折腾了个小站点，目前来说服务总体是稳定的，有冗余能实现低维护。
+  <br>功能上基本覆盖了日常，技术上也基本是目前最新最轻量的实现，只要网络状况不再度恶化（移动网确实垃圾，但是free is best！）就可以一直运行下去吧。
+  </div>
 </template>
 
 <style lang="scss" scoped>
